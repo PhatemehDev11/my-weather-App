aa9594042a02f03ff45392e7afae9cc7fe530faa
phatemeh amiri <fatemehamiri978@gmail.com>
2026-02-19 19:20:33 +0330

    add main page background

--- src/Page/WeatherPage.jsx
+++ src/Page/WeatherPage.jsx
@@ -2,7 +2,17 @@ function WeatherPage() {
 
     return(
    <>
-     
+     <div  className="
+       min-h-screen 
+      bg-gradient-to-br 
+      from-sky-300 
+      via-sky-200 
+      to-red-300 
+      flex 
+      flex-col 
+      items-center 
+      py-12
+     "></div>
    </>
     )
     
