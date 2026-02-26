5831d7b3842de302397ad2536b318b1d865aad82
phatemeh amiri <fatemehamiri978@gmail.com>
2026-02-27 00:36:15 +0330

    configure pwa plugin + add manifest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+
+      manifest: {
+        name: "Weather App",
+        short_name: "Weather",
+        description: "Modern weather app built with React & Tailwind",
+        theme_color: "#0ea5e9",
+        background_color: "#0ea5e9",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/7.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/8.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
