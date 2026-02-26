928febf9192ed8d376e26ea771695420b15f39cc
phatemeh amiri <fatemehamiri978@gmail.com>
2026-02-26 12:04:33 +0330

    render 5-day forecast from WeatherAPI

--- src/components/ForecastSection/Forecast.jsx
+++ src/components/ForecastSection/Forecast.jsx
@@ -1,18 +1,12 @@
-function Forecast() {
-    const days = [
-      { day: "Mon", temp: 22, icon: "🌧" },
-      { day: "Tue", temp: 28, icon: "☀️" },
-      { day: "Wed", temp: 25, icon: "☁️" },
-      { day: "Thu", temp: 27, icon: "🌤" },
-      { day: "Fri", temp: 23, icon: "⛈" },
-    ];
-  
-    return (
-      <div className="flex gap-6 mt-12">
-        {days.map((item, index) => (
-          <div
-            key={index}
-            className="
+function Forecast({ data }) {
+
+
+  return (
+    <div className="flex gap-6 mt-12">
+      {data.map((item) => (
+        <div
+          key={item.date}
+          className="
               w-28
               bg-white/30
               backdrop-blur-md
@@ -24,16 +18,22 @@ function Forecast() {
               hover:scale-105
               transition
             "
-          >
-            <p className="mb-2  text-sky-600 font-bold">{item.day}</p>
-            <p className="text-2xl">{item.icon}</p>
-            <p className="text-xl font-bold mt-2">{item.temp}°</p>
-          </div>
-        ))}
-      </div>
-    );
-  }
-  
+        >
+          <p className="mb-2  text-sky-600 font-bold">
+            {new Date(item.date).toLocaleDateString("en-US", {
+              weekday: "shotr",
+            })}</p>
+          <img
+            src={item.day.condition.icon}
+            alt={item.day.condition.text}
+            className="mx-auto" />
+          <p className="text-xl font-bold mt-2">{item.day.avgtemp_c}°</p>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 
-  export default Forecast
+
+export default Forecast
